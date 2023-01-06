db8bd9d7037729e86f2d458f37ba1730dc8f1b90
Jeong Wang <jeong@jeongs-mbp.myfiosgateway.com>
2023-01-06 13:01:44 -0500

    added readme and comments

--- rps.py
+++ rps.py
@@ -2,12 +2,12 @@ import random
 
 rps_list=['rock', 'paper', 'scissors']
 
+# Basic RPS function that meets all requirements
 def rps_player():
     while True:
         cp_choice=random.choice(rps_list)
         user_choice=input("Choose: Rock, Paper, Scissors or Quit ")
         
-
         if user_choice.lower() == 'rock':
             print("Computer chose:",cp_choice.title() )
             if cp_choice == 'rock':
@@ -39,20 +39,25 @@ def rps_player():
             print("Thank you for playing!")
             break
 
-        else:
+        else: # if player enter anything other than 'rock', 'paper', 'scissors', and 'quit', throw invalid input message
             print("Invalid input. Please try again.")
 
+# More complex RPS function
+# Player can now bet money
+# Player inputs the total amount of money they want to gamble
+# Each round, player can bet some or all of their money in their total amount
 def rps_player_2():
     player_pot = float(input("How much money will you be playing with today? $"))
-    win_count = 0
+    win_count = 0 # win count is used so if the player is winning a lot, they are set up to lose their next round >:D
 
     while True:
         print(f"Current player pot: ${player_pot}")
 
         if player_pot == 0:
-            print("Thank you for playing!")
+            print("You are out of funds. Thank you for playing!")
             break
-
+        
+        # loop used to make sure player does not bet over their total amount
         while True:
             bet_amount = float(input("How much do you want to bet this round? $"))
             if bet_amount > player_pot:
@@ -62,7 +67,8 @@ def rps_player_2():
 
         cp_choice=random.choice(rps_list)
         user_choice=input("Choose: Rock, Paper, Scissors or Quit ")        
-
+        
+        # if the player won 3 in a row, computer will automatically win the next round. "The house always wins!" >:D
         if user_choice.lower() == 'rock' and win_count == 3:
             print("Computer chose: Paper")
             print("You Lose!")
@@ -80,7 +86,9 @@ def rps_player_2():
             print("You Lose!")
             player_pot -= bet_amount
             win_count = 0
-
+        
+        # if the player wins, increase the player's pot by the player's bet amount and increment win count by 1
+        # if the player loses, decrease the player's pot by the player's bet amount and reset the win count to 0
         elif user_choice.lower() == 'rock':
             print("Computer chose:",cp_choice.title() )
             if cp_choice == 'rock':
